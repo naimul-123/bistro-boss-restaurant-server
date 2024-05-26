2033f48ee35a132c59d0dc8df55f3239ad10b6e2
Naimul Islam <naimuliu@gmail.com>
2024-05-26 09:19:35 +0600

    Cart delete done

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ require('dotenv').config()
 const port = process.env.PORT || 5000;
 username = process.env.DB_USER
 password = process.env.DB_PASS
-app.use(cors());
+app.use(cors({
+
+}));
+
+app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.send(`Surver is ok`)
 })
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${username}:${password}@cluster0.nevhe4f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,9 +44,23 @@ async function run() {
       const result = await reviewCollection.find().toArray();
       res.send(result)
     })
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email
+      const query = { email: email }
+      const result = await cartCollection.find(query).toArray();
+      res.send(result)
+    })
     app.post('/carts', async (req, res) => {
       const cartItem = req.body;
+      // console.log(cartItem);
       const result = await cartCollection.insertOne(cartItem);
+      res.send(result)
+    })
+    app.delete('/carts/:id', async (req, res) => {
+      const cartId = req.params.id;
+      const query = { _id: new ObjectId(cartId) }
+
+      const result = await cartCollection.deleteOne(query);
       res.send(result)
     })
 
